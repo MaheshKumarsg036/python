b2aa728d6400d6ae39ab54f2706f63d2af9aaca7
MaheshKumarsg036 <33930448+MaheshKumarsg1@users.noreply.github.com>
2019-08-29 09:40:18 +0530

    first commit inrto

---
+++ Basics/Intro/Introduction.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# ----- introduction to Python
+#object oriented interpreted Language*, allows multiple inheritance,
+#data in python is strongly typed 1+"hi" == error
+#data in python is strongly typed
+    # a= 2
+    # a= "hello"
+#python  is not backward compatability
+    
+print("hello world !") # function to print data to console
+print('hello world !')
+a = 5
+print() #prints blank line
+print(a+4)
+print("python's string easy to use") # start a string with one type finish it with the same type
+print("hello"+" world!") # string concatenation
+greeting = "hello"
+name = " mahesh"
+print(greeting+name)
+
+    
+    
---
+++ Basics/Introduction.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 08:39:22 2019
+
+@author: maheshkumar
+"""
+
